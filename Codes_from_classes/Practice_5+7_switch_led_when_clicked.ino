b12bb23c6b3e1fadc5531d4fcbbf423b8cfdb10f
andr878 <andr99@live.dk>
2025-10-01 09:32:48 +0200

    Create Practice_5+7_switch_led_when_clicked.ino

---
+++ Codes_from_classes/Practice_5+7_switch_led_when_clicked.ino
@@ -0,0 +1,97 @@
+const int rLEDpin = 3;
+const int bLEDpin = 4;
+const int gLEDpin = 5;
+const int pushButton = 2;
+int buttonState = 0;
+int ledcolor = 0;
+String color; 
+bool buttonPressed = false;
+
+
+void setup(){
+  Serial.begin(9600);
+  pinMode(rLEDpin, OUTPUT);
+  pinMode (bLEDpin, OUTPUT);
+  pinMode (gLEDpin, OUTPUT);
+  pinMode(pushButton,INPUT);
+}
+
+void loop(){
+  buttonState = digitalRead(pushButton);
+  Serial.println(color);
+  if (buttonState == HIGH && !buttonPressed) {
+    ledcolor = ledcolor + 1;
+    buttonPressed = true;
+    //delay(100);
+  }
+  if (buttonState == LOW && buttonPressed) {
+    buttonPressed = false;
+  }
+
+  if (ledcolor == 0) {
+      digitalWrite(rLEDpin, HIGH);
+      digitalWrite(bLEDpin, HIGH);
+      digitalWrite(gLEDpin, HIGH);
+      
+    }
+
+  else if (ledcolor == 1){
+    //red
+      digitalWrite(rLEDpin, LOW);
+      digitalWrite(bLEDpin, HIGH);
+      digitalWrite(gLEDpin, HIGH);
+      color = "red";
+  }
+
+ else if (ledcolor == 2){
+    //green
+      digitalWrite(rLEDpin, HIGH);
+      digitalWrite(bLEDpin, HIGH);
+      digitalWrite(gLEDpin, LOW);
+      color = "green";
+  }
+
+   else if (ledcolor == 3){
+    //BLUE
+      digitalWrite(rLEDpin, HIGH);
+      digitalWrite(bLEDpin, LOW);
+      digitalWrite(gLEDpin, HIGH);
+      color = "blue";
+  }
+
+   else if (ledcolor == 4){
+    //Yellow
+      digitalWrite(rLEDpin, LOW);
+      digitalWrite(bLEDpin, HIGH);
+      digitalWrite(gLEDpin, LOW);
+      color = "yellow";
+  }
+
+   else if (ledcolor == 5){
+    //PURPLE
+      digitalWrite(rLEDpin, LOW);
+      digitalWrite(bLEDpin, LOW);
+      digitalWrite(gLEDpin, HIGH);
+      color = "purple";
+  }
+
+   else if (ledcolor == 6){
+    //CYAN
+      digitalWrite(rLEDpin, HIGH);
+      digitalWrite(bLEDpin, LOW);
+      digitalWrite(gLEDpin, LOW);
+      color = "cyan";
+  }
+
+   else if (ledcolor == 7){
+    //WHITE
+      digitalWrite(rLEDpin, LOW);
+      digitalWrite(bLEDpin, LOW);
+      digitalWrite(gLEDpin, LOW);
+      color = "white";
+  }
+
+   else if (ledcolor == 8){
+    ledcolor =0;
+}
+}
